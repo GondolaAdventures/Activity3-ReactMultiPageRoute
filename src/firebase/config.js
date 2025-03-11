b16e10cf039cec715bf3cc96d0c0ca7e20e7eed2
Gondola's Adventure <83150401+GondolaAdventures@users.noreply.github.com>
2025-03-11 23:41:37 +0800

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import {getFirestore} from 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhCBiX7VBhiRft0RxCFJp1OodJD_qzJ5U",
-  authDomain: "articledemo-5edb7.firebaseapp.com",
-  projectId: "articledemo-5edb7",
-  storageBucket: "articledemo-5edb7.firebasestorage.app",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
   messagingSenderId: "1020340575348",
-  appId: "1:1020340575348:web:86781eaa246596d1668521"
+  appId: "1020340575348"
 };
 
   initializeApp(firebaseConfig);
